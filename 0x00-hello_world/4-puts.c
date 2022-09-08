170501e01aa9fddaf02bd07622296b49d87834cd
Crizy27 <crisssamuelserunjogi@gmail.com>
2022-09-08 23:01:14 +0000

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,11 +2,11 @@
 
 /**
  * main - This is a sinple c program
- * return: it returns something
+ * Return: it returns something
  */
 
 int main(void)
 {
-	puts("\"Programming is like building a multilingula puzzle");
-	return(0);
-}
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}	
