e8de6cd9a67009ec0c10442bfc7eb131a9169306
Crizy27 <crisssamuelserunjogi@gmail.com>
2022-09-08 22:42:32 +0000

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - This is a sinple c program
+ * return: it returns something
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingula puzzle");
+	return(0);
+}
