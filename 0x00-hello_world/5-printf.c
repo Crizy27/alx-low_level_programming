71334d934d1d63e17f8bc18f40cf4f122ceb3d24
Crizy27 <crisssamuelserunjogi@gmail.com>
2022-09-09 00:18:44 +0000

    5-print.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - This is a simple c program
+ * return: It returns something
+ */
+int main(void)
+{
+        printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}	
