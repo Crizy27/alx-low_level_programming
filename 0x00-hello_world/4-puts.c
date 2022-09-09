b9bf4e3cb9185d8fbdb5b31d21f17116f3d39dc1
Crizy27 <crisssamuelserunjogi@gmail.com>
2022-09-09 03:03:17 +0000

    4-put.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - This is a simple c program
- * Return: It returns something
- */
-
+*main - This is a comment
+*The comments still goes here
+*Return: This marks the end of the function
+*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-}	
+}
