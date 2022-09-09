181adecd8fd367eb02ced322b533b0f4bccba72a
Crizy27 <crisssamuelserunjogi@gmail.com>
2022-09-09 02:05:12 +0000

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,11 +2,11 @@
 
 /**
  *main - Use printf instead of puts
- *Return: It returns something
+ *
+ *Return: printf prints the long output
  */
-
-int main(0)
+ int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }	
