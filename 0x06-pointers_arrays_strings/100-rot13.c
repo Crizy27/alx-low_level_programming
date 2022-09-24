c14673dc60b15d00b532347d4d21c5440a2b5c2d
Crizy27 <crisssamuelserunjogi@gmail.com>
2022-09-24 15:42:33 +0000

    100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,46 +1,30 @@
 #include "main.h"
 
 /**
- * roti3 - rot13 encoder
- * @str string to be encoded
+ * rot13 - encodes a string in rot13
+ * @s: string to be encoded
  *
- * Return: address of the encoded string
+ * Return: the resulting string
  */
 
-char *rot13(char *str)
+char *rot13(char *s)
 {
-	int i = 0;
+	int i, j;
 
-	while (str[i] != '\0')
+	char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char b[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; a[i] != '\0'; i++)
 	{
-		str[i] = transform_2(str[i]);
-		i++;
-	}
-	return (str);
-}
-
-/**
- * transform_2 - helper function to map a letter with it's rot13 encoding
- * @x: char to be ecoded char
- *
- * Return: the encoded char
- */
-
-char transform_2(char x)
-{
-	char one[52] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-	char two[52] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
-	int i = 0;
-	char replacement = x;
-
-		while (i < 52)
+		for (j = 0; a[j] != '\0'; j++)
 		{
-			if (x == one[i])
+			if (s[i] == a[j])
 			{
-				replacement = two[i];
+				s[i] = b[j];
 				break;
 			}
-			i++;
 		}
-	return (replacement);
+	}
+
+	return (s);
 }
