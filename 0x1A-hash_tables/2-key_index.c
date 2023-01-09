46ace5c9a30306f478fe5099d4e9709fea5a108d
Crizy27 <crisssamuelserunjogi@gmail.com>
2023-01-09 19:54:14 +0000

    2-key_index.c

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,19 +1,12 @@
-#include <stdlib.h>
+#include "hash_tables.h"
 
-unsigned long int hash_djb2(const unsigned char *str) 
-{
-    unsigned long int hash = 5381;
-    int c;
-
-    while ((c = *str++)) 
-    {
-        hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
-    }
-
-    return hash;
-}
-
+/**
+ * key_index - function that get the index where key/value is stored
+ * @key: type pointer char of key index
+ * @size: type pointer char the size of the array
+ * Return: Index of the key
+ */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-    return hash_djb2(key) % size;
+	return (hash_djb2(key) % size);
 }
